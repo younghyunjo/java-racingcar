83fec8c45a3de5f6c053b8bf952fb85166815074
Younghyun <younghyunjo99@gmail.com>
2021-03-10 18:46:52 +0900

    feat(step4):print winner name

--- src/main/java/RacingCar/InputView.java
+++ src/main/java/RacingCar/InputView.java
@@ -1,7 +1,21 @@
 package RacingCar;
 
-public interface InputView<T> {
-    T getCarInfo();
+import java.util.Scanner;
 
-    int getNumberOfRacing();
+public class InputView {
+    private Scanner scanner = new Scanner(System.in);
+
+    public int getNumberOfCars() {
+        final String QUESTION_FOR_GET_NUMBER_OF_CARS = "자동차 대수는 몇 대 인가요?";
+        System.out.println(QUESTION_FOR_GET_NUMBER_OF_CARS);
+        int numberOfCars = scanner.nextInt();
+        return numberOfCars;
+    }
+
+    public int getNumberOfRacing() {
+        final String QUESTION_FOR_GET_NUMBER_OF_RACING = "시도할 회수는 몇 회 인가요?";
+        System.out.println(QUESTION_FOR_GET_NUMBER_OF_RACING);
+        int numberOfRacing = scanner.nextInt();
+        return numberOfRacing;
+    }
 }
